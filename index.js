a3a41b7184731529ea84e28f4168a247dd53a1ba
BanulaMahen <banulakumarage@gmail.com>
2022-12-03 01:59:34 +0530

    thumbnail and bugs fixing

--- index.js
+++ index.js
@@ -51,14 +51,14 @@ app.get("/getVideos", async (req,res) =>  {
 
 app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
-    console.log(req.body.title);
+    let fileid = 15;
     const file = req.file.filename ;
     if (err) {
       console.log("Error while uploading");
     }
     res.send("Video processing");
     let filename = file.split(".")[0];
-    const folderName = `segments/${filename}`;
+    const folderName = `segments/${fileid}`;
     try {
       if (!fs.existsSync(folderName)) {
         fs.mkdirSync(folderName);
@@ -66,9 +66,14 @@ app.post("/upload", (req, res) => {
     } catch (err) {
       console.error(err);
     }
-    filename = filename.split(".")[0];
-    let cmd = `ffmpeg -re -i ./uploads/${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:0 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${filename}/${filename}_out.mpd`;
 
+    filename = filename.split(".")[0];
+    console.log(filename)
+    let thumbnailcmd = `ffmpeg -i ./uploads/${filename}.mp4 -vf "select=eq(n\,0)" -q:v 3 ./thumbnails/${fileid}.jpg`
+    exec(thumbnailcmd)
+
+    let cmd = `ffmpeg -re -i ./uploads/${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:0 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${fileid}/${fileid}_out.mpd`;
+    
     exec(cmd, (err, output) => {
       if (err) {
         console.error("could not execute command: ", err);
