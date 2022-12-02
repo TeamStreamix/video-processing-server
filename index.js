f6239a0f2302c2a4b8fb4cfeb774c8093a7b02a3
BanulaMahen <banulakumarage@gmail.com>
2022-12-02 19:35:36 +0530

    file upload handled

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
 
 const app = express()
+
+const storage = multer.diskStorage({   
+    destination: function(req, file, cb) { 
+       cb(null, path.join(__dirname, 'uploads'));    
+    }, 
+    filename: function (req, file, cb) { 
+       cb(null , file.originalname);   
+    }
+ });
+
+const upload = multer({storage: storage}).single('video')
+
 
 const { exec } = require('node:child_process')
 
@@ -9,27 +23,32 @@ app.get('/',(req, res)=>{
     res.send("MPD generator")
 })
 
-app.get('/mpd',(req,res)=>{
-    let filename = req.query.filename
-    res.send('File is processing')
-    const folderName = `segments/${filename}`;
-    try {
-        if (!fs.existsSync(folderName)) {
-            fs.mkdirSync(folderName);
-        }
-    } catch (err) {
-        console.error(err);
-    }
-
-    let cmd = `ffmpeg -re -i ./uploads/${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:0 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${filename}/${filename}_out.mpd`
-
-    exec(cmd, (err, output) => {
+app.post('/upload',(req,res)=>{
+    upload(req, res, (err)=> {
+        let file = req.file.filename;
         if (err) {
-            console.error("could not execute command: ", err)
-            return
+            console.log("Error while uploading");
         }
-        console.log("Mpd file has been generated");
-        
+        res.send("Video processing");
+        let filename = file.split('.')[0]
+        const folderName = `segments/${filename}`;
+        try {
+            if (!fs.existsSync(folderName)) {
+                fs.mkdirSync(folderName);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+        filename = filename.split('.')[0]
+        let cmd = `ffmpeg -re -i ./uploads/${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:0 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${filename}/${filename}_out.mpd`
+    
+        exec(cmd, (err, output) => {
+            if (err) {
+                console.error("could not execute command: ", err)
+                return
+            }
+            console.log("Mpd file has been generated");
+        })
     })
 })
 
