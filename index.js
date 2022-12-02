8899c753cac3b56e049bd0ec9b6c4d55bb1a7bac
BanulaMahen <banulakumarage@gmail.com>
2022-12-02 18:49:05 +0530

    bugs fixing

--- index.js
+++ index.js
@@ -20,17 +20,14 @@ app.get('/mpd',(req,res)=>{
     } catch (err) {
         console.error(err);
     }
-    // let cmd = 'ffmpeg -re -i ./a.mp4 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:0 800k -b:v:1 300k -s:v:1 320x170 -profile:v:1 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -window_size 5 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./b.mpd'
-    let cmd = `ffmpeg -re -i ./${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:1 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${filename}/${filename}_out.mpd`
+
+    let cmd = `ffmpeg -re -i ./uploads/${filename}.mp4 -map 0 -map 0 -map 0 -c:a aac -c:v libx264 -b:v:1 800k -b:v:2 500k -s:v:0 1920x1080 -s:v:1 1280x720 -s:v:2 720x480 -profile:v:1 baseline -profile:v:2 baseline -profile:v:0 main -bf 1 -keyint_min 120 -g 120 -sc_threshold 0 -b_strategy 0 -ar:a:1 22050 -use_timeline 1 -use_template 1 -adaptation_sets "id=0,streams=v id=1,streams=a" -f dash ./segments/${filename}/${filename}_out.mpd`
 
     exec(cmd, (err, output) => {
-        // once the command has completed, the callback function is called
         if (err) {
-            // log and return if we encounter an error
             console.error("could not execute command: ", err)
             return
         }
-        // log the output received from the command
         console.log("Mpd file has been generated");
         
     })
